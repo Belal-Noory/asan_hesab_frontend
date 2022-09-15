5f9b125eb74590af81018fdc22b5015d4f037e0b
Belal Noory <belalnoory2@gmail.com>
2022-09-15 03:12:46 -0700

    updated

--- src/DBconfig.js
+++ src/DBconfig.js
@@ -1,13 +1,13 @@
-// export const LOGIN_URL = "https://asan-hesab.herokuapp.com/admin/auth";
-// export const CUSTOMERS_URL = "https://asan-hesab.herokuapp.com/customers";
-// export const EXPENSES_URL = "https://asan-hesab.herokuapp.com/expenses";
-// export const HOLDERS_URL = "https://asan-hesab.herokuapp.com/shareholder";
-// export const TRANSACTIONS_URL = "https://asan-hesab.herokuapp.com/transactions";
-// export const WITHDRAWS_URL = "https://asan-hesab.herokuapp.com/withdraw";
+export const LOGIN_URL = "https://asan-hesab.herokuapp.com/admin/auth";
+export const CUSTOMERS_URL = "https://asan-hesab.herokuapp.com/customers";
+export const EXPENSES_URL = "https://asan-hesab.herokuapp.com/expenses";
+export const HOLDERS_URL = "https://asan-hesab.herokuapp.com/shareholder";
+export const TRANSACTIONS_URL = "https://asan-hesab.herokuapp.com/transactions";
+export const WITHDRAWS_URL = "https://asan-hesab.herokuapp.com/withdraw";
 
-export const LOGIN_URL = "http://localhost:5050/admin/auth";
-export const CUSTOMERS_URL = "http://localhost:5050/customers";
-export const EXPENSES_URL = "http://localhost:5050/expenses";
-export const HOLDERS_URL = "http://localhost:5050/shareholder";
-export const TRANSACTIONS_URL = "http://localhost:5050/transactions";
-export const WITHDRAWS_URL = "http://localhost:5050/withdraw";
+// export const LOGIN_URL = "http://localhost:5050/admin/auth";
+// export const CUSTOMERS_URL = "http://localhost:5050/customers";
+// export const EXPENSES_URL = "http://localhost:5050/expenses";
+// export const HOLDERS_URL = "http://localhost:5050/shareholder";
+// export const TRANSACTIONS_URL = "http://localhost:5050/transactions";
+// export const WITHDRAWS_URL = "http://localhost:5050/withdraw";
